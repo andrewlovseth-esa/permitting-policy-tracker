ebb657834f7c2fa5e2a6412bd05bd40e046b7c4e
Andrew Lovseth <113471723+andrewlovseth-esa@users.noreply.github.com>
2025-03-28 13:32:33 -0700

    Update JS versions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-import { setupMobileNav, setupMobileFilterToggle } from "./mobileNav.js?v=1.1";
-import { setupActions } from "./actions.js?v=1.1";
-import { setupSubComponentFilter } from "./sub-components.js?v=1.1";
+import { setupMobileNav, setupMobileFilterToggle } from "./mobileNav.js?v=1.2";
+import { setupActions } from "./actions.js?v=1.2";
+import { setupSubComponentFilter } from "./sub-components.js?v=1.2";
 
 // Initialize mobile navigation logic
 setupMobileNav();
