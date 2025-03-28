4946f5ab94a1602c758151bf61c4222e90dc48ec
Andrew Lovseth <113471723+andrewlovseth-esa@users.noreply.github.com>
2025-03-28 12:23:03 -0700

    Update actions structure

--- templates/home.php
+++ templates/home.php
@@ -10,13 +10,8 @@ get_header(); ?>
 
     <?php  get_template_part('templates/home/hero'); ?>
 
+    <?php get_template_part('templates/home/filters'); ?>
 
-    <section id="actions">
-        <?php get_template_part('templates/home/filters'); ?>
-
-        <?php get_template_part('templates/home/list'); ?>
-    </section>
-
-
+    <?php get_template_part('templates/home/list'); ?>
 
 <?php get_footer(); ?>
